c254d8fedcd543aca265d61bb907d86abb245cd1
sandeepkumar <sandeepkr11320@gmail.com>
2024-09-21 15:01:26 +0530

    Added playlist feature and storing it to firebase

--- app/src/main/java/com/kittunes/ActivityDrawer.kt
+++ app/src/main/java/com/kittunes/ActivityDrawer.kt
@@ -9,11 +9,13 @@ import com.kittunes.databinding.ActivityDrawerBinding
 
 class ActivityDrawer : AppCompatActivity() {
     private lateinit var binding: ActivityDrawerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityDrawerBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
